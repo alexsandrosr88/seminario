205a78af775e5fca28e5e808aec93609bff9d1cf
Alexsandro Ramos <alexsandrosr@msn.com>
2021-11-07 23:03:29 -0300

    primeiro commit

---
+++ demo/src/main/java/com/example/demo/entities/FuncionarioEntity.java
@@ -0,0 +1,55 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "funcionario")
+public class FuncionarioEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	
+	private String telefone;
+	
+	public FuncionarioEntity() {}
+
+	public FuncionarioEntity(Integer id, String nome, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	
+}
